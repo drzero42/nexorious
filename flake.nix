7c01b6711df5b0a82ab4eaa347b276003f3a02d2
Anders Bøgh Bruun <anders@boghbruun.dk>
2026-05-21 20:16:05 +0200

    chore(nix): pin nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nexorious — self-hosted game collection manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
