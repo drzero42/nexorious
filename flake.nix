f17b9822813db082b6e592f105f3f7d60f0ebf70
Anders Bøgh Bruun <anders@boghbruun.dk>
2026-05-31 19:55:12 +0200

    chore(nix): Upgrade flake to use nixpkgs/nixos-26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nexorious — self-hosted game collection manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
